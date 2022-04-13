e7ef300b820e0654be550db22e69180b1c861c46
SAMSULYA <sulsam2000@gmail.com>
2022-04-13 20:58:55 +0700

    table 1

---
+++ database/migrations/2022_04_13_134604_create_vehicles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama-kendaraan');
+            $table->date('jadwal-service');
+            $table->integer('konsumsi-bbm');
+            $table->string('satuan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
